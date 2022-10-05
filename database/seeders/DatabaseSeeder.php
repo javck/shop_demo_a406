38b8a03963abb3588c0e211857b29da41ce7aa4b
品爵老師 <javck.zack@gmail.com>
2022-10-05 11:46:23 +0800

    檔案優化

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,13 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Schema::disableForeignKeyConstraints();
-        \App\Models\User::truncate();
-        \App\Models\User::factory(10)->create();
-        
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(UserSeeder::class);
         $this->call(ItemSeeder::class);
         Schema::enableForeignKeyConstraints();
     }
