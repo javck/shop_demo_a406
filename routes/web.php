e63fc2b22bcf642696dc13e210c62557795fb8f9
品爵老師 <javck.zack@gmail.com>
2022-10-05 11:08:57 +0800

    初始化

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+Route::middleware(['auth'])->group(function(){
+//保護某些路由需要登入才能訪問
+    Route::get('/vip',function(){
+        return 'VIP Zone';
+    });
+});
+
